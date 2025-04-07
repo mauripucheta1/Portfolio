98903c6670fa47d1356b9cbdde3bf34b9ac7721e
Mauricio Pucheta <mauriciopucheta21@gmail.com>
2025-04-07 17:04:16 -0300

    Edición: Se modificaron archivos tales como: 1) Temas.js: Se modificó con el fin de reemplazar las imágenes al aplicarse el "Modo Oscuro" y viceversa.

--- public/Temas.js
+++ public/Temas.js
@@ -4,26 +4,42 @@ const buttonThemeDark = document.getElementById("buttonThemeDark");
 const sections = document.querySelectorAll(".sectionDark");
 const gradient = document.querySelector('.sectionDarkGradient');
 const cards = document.querySelectorAll('.contenedorTarjeta');
+const heroImageDesktop = document.querySelector('.ocultar-en-telefono');
+const heroImageMobile = document.querySelector('.ocultar-en-pc');
 const body = document.body;
 
 //Función para activar el modo oscuro
 buttonThemeDark.addEventListener("click", () => {
+
     sections.forEach((section) => {
         section.classList.add('dark-theme');
     });
+
     gradient.classList.add('gradientDark');
+
     cards.forEach((card) => {
         card.classList.add('borderDark');
     })
+
     body.classList.add('dark-theme');
+
+    heroImageDesktop.src = "img/bg-HeroSectionDark.png"; 
+    heroImageMobile.src = "img/bg-HeroSection2Dark.png"; 
 });
 
 //Función para desactivar el modo oscuro
 buttonThemeLight.addEventListener("click", () => {
+
     sections.forEach((section) => {
         section.classList.remove('dark-theme');
         section.classList.add('light-theme');
     });
+
     gradient.classList.remove('gradientDark');
+    
     body.classList.remove('dark-theme');
+
+    heroImageDesktop.src = "img/bg-HeroSection.png";
+    heroImageMobile.src = "img/bg-HeroSection2.png"; 
+
 });
